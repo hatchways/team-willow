c7087dffdc06464325f438a5fc9d89c371472611
OsmanRana <osmancmc@yahoo.com>
2022-01-15 20:03:24 +0600

    AuthPageWrapper removed

--- client/src/pages/Settings/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/pages/Settings/ProfilePhoto/ProfilePhoto.tsx
@@ -3,7 +3,6 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import useStyles from './useStyles';
-import AuthPageWrapper from '../../../components/AuthPageWrapper/AuthPageWrapper';
 import { Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FormLabel from '@mui/material/FormLabel';
@@ -21,36 +20,34 @@ const ProfilePhoto: React.FC<ProfilePhotoProps> = ({ header }) => {
   return (
     <>
       <SettingHeader header={header} />
-      <AuthPageWrapper header="Profile Photo">
-        <Box textAlign="center">
-          <Box className={classes.imageCropper}>
-            <img
-              className={classes.img}
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-              alt="Profile Photo"
-            />
-          </Box>
-          <Typography variant="body1" className={classes.txt}>
-            Be sure to use a photo that <br /> clearly shows your face
-          </Typography>
-          <Box className={classes.input}>
-            <FormLabel htmlFor="contained-button-file">
-              <Input accept="image/*" id="contained-button-file" multiple type="file" />
-              <Button variant="outlined" component="span" fullWidth>
-                <Typography variant="h6" className={classes.btnTxt}>
-                  Upload a file from your device
-                </Typography>
-              </Button>
-            </FormLabel>
-          </Box>
-          <Button aria-label="delete">
-            <DeleteIcon sx={{ mr: 1, color: 'black' }} />
-            <Typography variant="body1" className={classes.txt}>
-              Delete photo
-            </Typography>
-          </Button>
+      <Box textAlign="center">
+        <Box className={classes.imageCropper}>
+          <img
+            className={classes.img}
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            alt="Profile Photo"
+          />
         </Box>
-      </AuthPageWrapper>
+        <Typography variant="body1" className={classes.txt}>
+          Be sure to use a photo that <br /> clearly shows your face
+        </Typography>
+        <Box className={classes.input}>
+          <FormLabel htmlFor="contained-button-file">
+            <Input accept="image/*" id="contained-button-file" multiple type="file" />
+            <Button variant="outlined" component="span" fullWidth>
+              <Typography variant="h6" className={classes.btnTxt}>
+                Upload a file from your device
+              </Typography>
+            </Button>
+          </FormLabel>
+        </Box>
+        <Button aria-label="delete">
+          <DeleteIcon sx={{ mr: 1, color: 'black' }} />
+          <Typography variant="body1" className={classes.txt}>
+            Delete photo
+          </Typography>
+        </Button>
+      </Box>
     </>
   );
 };
