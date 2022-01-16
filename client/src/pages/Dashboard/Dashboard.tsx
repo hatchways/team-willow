d58fea98c6b3009625ae19cf2a214a162a2fc6f3
OsmanRana <osmancmc@yahoo.com>
2022-01-16 12:32:27 +0600

    mock user adde

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -5,6 +5,63 @@ import { useHistory } from 'react-router-dom';
 import { CircularProgress, Grid, Typography } from '@mui/material';
 import PageContainer from '../../components/PageContainer/PageContainer';
 
+//mock user data
+const users = [
+  {
+    id: 1,
+    name: 'Norma Byers',
+    occupation: 'Loving pet sitter',
+    rating: 4,
+    description: 'Dog sitting, cat sitting, pocket pet and bird care',
+    rate: 14,
+    city: 'Toronto, Ontario',
+  },
+  {
+    id: 2,
+    name: 'Michael Carnahan',
+    occupation: 'Professional dog trainer',
+    rating: 5,
+    description: 'I would love to work with your dog',
+    rate: 20,
+    city: 'Toronto, Ontario',
+  },
+  {
+    id: 3,
+    name: 'Jessica Pearson',
+    occupation: 'Dog care helper',
+    rating: 5,
+    description: 'I have had dogs as pets for most of my life',
+    rate: 15,
+    city: 'Toronto, Ontario',
+  },
+  {
+    id: 1,
+    name: 'Norma Byers',
+    occupation: 'Loving pet sitter',
+    rating: 4,
+    description: 'Dog sitting, cat sitting, pocket pet and bird care',
+    rate: 14,
+    city: 'Toronto, Ontario',
+  },
+  {
+    id: 2,
+    name: 'Michael Carnahan',
+    occupation: 'Professional dog trainer',
+    rating: 5,
+    description: 'I would love to work with your dog',
+    rate: 20,
+    city: 'Toronto, Ontario',
+  },
+  {
+    id: 3,
+    name: 'Jessica Pearson',
+    occupation: 'Dog care helper',
+    rating: 5,
+    description: 'I have had dogs as pets for most of my life',
+    rate: 15,
+    city: 'Toronto, Ontario',
+  },
+];
 export default function Dashboard(): JSX.Element {
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
