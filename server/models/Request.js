efddb3cd1a561f6baf4f7ed043b0133fa8ead39c
Jacob Tingling <jacob.tingling@protonmail.com>
2022-01-16 13:38:20 -0500

    Issue #9: Request Model

---
+++ server/models/Request.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    sitterId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    date: { 
+        type: Date, 
+        required: true
+    },
+    start: { 
+        type: String, 
+        required: true,
+    },
+    end: { 
+        type: String, 
+        required: true,
+    },
+    accepted: { 
+        type: Boolean,
+        default: false,
+    },
+    declined: { 
+        type: Boolean,
+        default: false,
+    },
+    paid: { 
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = Request = mongoose.model("Request", requestSchema);
