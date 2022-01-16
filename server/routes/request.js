96c73926f7ffcc01fcf652925aefd7db36d0624c
Jacob Tingling <jacob.tingling@protonmail.com>
2022-01-16 14:54:20 -0500

    Issue #8: Request Controller

---
+++ server/routes/request.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const protect = require("../middleware/auth");
+const { getRequests, createRequest, editRequest } = require("../controllers/request");
+
+router.route("/get").get(protect, getRequests);
+router.route("/create").post(protect, createRequest);
+router.route("/edit").put(protect, editRequest);
+
+module.exports = router;
