df4b34f4c06bd38eae02dcf0b1557d981c532362
Manan Bhavsar <mananbh9@live.com>
2022-01-19 11:45:59 -0500

    removed Demo Sign Up button

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -6,54 +6,6 @@ import { useSnackBar } from '../../context/useSnackbarContext';
 import AuthPageWrapper from '../../components/AuthPageWrapper/AuthPageWrapper';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import AuthPageFooter from '../../components/AuthPageFooter/AuthPageFooter';
-import Button from '@mui/material/Button';
-import { FetchOptions } from '../../interface/FetchOptions';
-import useStyles from './useStyles';
-import Box from '@mui/material/Box';
-
-function DemoSignUp(): JSX.Element {
-  const { updateLoginContext } = useAuth();
-  const { updateSnackBarMessage } = useSnackBar();
-  const classes = useStyles();
-
-  const handleSubmit = async () => {
-    const fetchOptions: FetchOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
-    };
-    return await fetch(`/auth/demo/signup`, fetchOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.error) {
-          updateSnackBarMessage(data.error.message);
-        } else if (data.success) {
-          updateSnackBarMessage(data.success);
-          updateLoginContext(data.success);
-        } else {
-          console.log({ data });
-          updateSnackBarMessage('unexpected error occurred!');
-        }
-      })
-      .catch(() => ({
-        error: { message: 'Unable to connect to server. Please try again' },
-      }));
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <Button
-        className={classes.demoButton}
-        type="submit"
-        size="large"
-        variant="contained"
-        color="primary"
-        disableElevation
-      >
-        Demo SignUp
-      </Button>
-    </form>
-  );
-}
 
 export default function Register(): JSX.Element {
   const { updateLoginContext } = useAuth();
@@ -84,7 +36,6 @@ export default function Register(): JSX.Element {
     <PageContainer>
       <AuthPageWrapper header="Sign up">
         <SignUpForm handleSubmit={handleSubmit} />
-        <DemoSignUp />
         <AuthPageFooter text="Already a member?" anchorText="Login" anchorTo="/login" />
       </AuthPageWrapper>
     </PageContainer>
