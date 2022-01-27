94d3b86b9ccb6058190b67e8db630147cdf01fad
Jacob Tingling <jacob.tingling@protonmail.com>
2022-01-27 14:55:40 -0500

    payment method is now sent in req body

--- server/test/confirmPaymentIntents.spec.js
+++ server/test/confirmPaymentIntents.spec.js
@@ -12,8 +12,9 @@ describe("confirm Payment Intents", function () {
     const token = `token=${process.env.TOKEN}`;
     it('should confirm a payment', (done) => {
         chai.request(`http://localhost:3001/payments/${paymentId}`)
-        .get('/pay')
+        .post('/pay')
         .set('Cookie', token)
+        .send({ paymentMethod: 'pm_card_visa', })
         .end(function(err, res) {
             expect(res).to.have.status(200); 
             expect(res.body.success).to.be.eql('succeeded');
